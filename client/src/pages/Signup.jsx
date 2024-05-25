8f0476753978dc9001fd95b15d99a9df4a23c592
Khanna12345678 <131513389+Khanna12345678@users.noreply.github.com>
2024-05-25 15:02:41 +0530

    add funcionality to the signup page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,14 +1,43 @@
-import React from 'react';
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
-import { Link } from 'react-router-dom';
-// import OAuth from '../components/OAuth';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
-export default function Signup() {
-  // Placeholder for the loading state to demonstrate UI
-  const loading = false;
+export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-  // Placeholder for the error message state to demonstrate UI
-  const errorMessage = null;
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage('Please fill out all fields.');
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if (res.ok) {
+        navigate('/sign-in');
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
 
   return (
     <div className='min-h-screen mt-20'>
@@ -28,13 +57,14 @@ export default function Signup() {
         </div>
         {/* right */}
         <div className='flex-1'>
-          <form className='flex flex-col gap-4' onSubmit={(e) => e.preventDefault()}>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
             <div>
               <Label value='Your username' />
               <TextInput
                 type='text'
                 placeholder='Username'
                 id='username'
+                onChange={handleChange}
               />
             </div>
             <div>
@@ -43,6 +73,7 @@ export default function Signup() {
                 type='email'
                 placeholder='name@company.com'
                 id='email'
+                onChange={handleChange}
               />
             </div>
             <div>
@@ -51,6 +82,7 @@ export default function Signup() {
                 type='password'
                 placeholder='Password'
                 id='password'
+                onChange={handleChange}
               />
             </div>
             <Button
@@ -67,7 +99,6 @@ export default function Signup() {
                 'Sign Up'
               )}
             </Button>
-            {/* <OAuth /> */}
           </form>
           <div className='flex gap-2 text-sm mt-5'>
             <span>Have an account?</span>
@@ -85,9 +116,3 @@ export default function Signup() {
     </div>
   );
 }
-
-
-
-
-
-

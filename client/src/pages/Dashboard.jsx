aedf30b356af8c4bc42edd867608728aa43d6ba9
Khanna12345678 <131513389+Khanna12345678@users.noreply.github.com>
2024-05-26 17:08:22 +0530

    complete sidebar of the dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,12 +1,40 @@
-// src/pages/Dashboard.jsx
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import DashSidebar from './DashSidebar';
+import DashProfile from './DashProfile';
+import DashPosts from './DashPosts';
+import DashUsers from './DashUsers';
+import DashComments from './DashComments';
+import DashboardComp from './DashboardComp';
 
-function Dashboard() {
+
+
+export default function Dashboard() {
+  const location = useLocation();
+  const [tab, setTab] = useState('');
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      <div className='md:w-56'>
+        {/* Sidebar */}
+        <DashSidebar />
+      </div>
+      {/* profile... */}
+      {tab === 'profile' && <DashProfile />}
+      {/* posts... */}
+      {tab === 'posts' && <DashPosts />}
+      {/* users */}
+      {tab === 'users' && <DashUsers />}
+      {/* comments  */}
+      {tab === 'comments' && <DashComments />}
+      {/* dashboard comp */}
+      {tab === 'dash' && <DashboardComp />}
     </div>
   );
 }
-
-export default Dashboard;

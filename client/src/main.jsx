96fef2687142c2845de7b7ef41ce0fa42c2fe633
Khanna12345678 <131513389+Khanna12345678@users.noreply.github.com>
2024-05-26 13:18:06 +0530

    add redux persisit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,14 +4,20 @@ import App from './App.jsx'
 import './index.css'
 
 import { Provider } from 'react-redux'
-import { store } from './redux/store.js'
+import { store,persistor } from './redux/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+   
+  <PersistGate persistor={persistor}>
+
+
   <Provider store={store}>
     
   <App />
   
   
   </Provider>
+  </PersistGate>
     
 )
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,18 +1,44 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore ,combineReducers} from '@reduxjs/toolkit'
 import userSlice  from './user/userSlice'
+import {persistReducer,persistStore} from "redux-persist";
+import storage from 'redux-persist/lib/storage';
+import { version } from 'mongoose';
+  
+const rootReducer=combineReducers({
+  
+    user:userSlice,
 
 
-export const store = configureStore({
-  reducer: {
-     
- user:userSlice,
+
+
+
+});
+
     
+    const persistConfig={
+        key:'root',
+        storage,
+        version: 1,
+
+
+
+    };
+
+  const persistedReducer=persistReducer(persistConfig,rootReducer);
 
 
 
 
-  },
-})
+
+//   const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+  export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({ serializableCheck: false }),
+  });
+  
+  export const persistor = persistStore(store);
 
 // // Infer the `RootState` and `AppDispatch` types from the store itself
 // export type RootState = ReturnType<typeof store.getState>

5eda80a2eda6ff94d9f27902daa6f658ff8f5842
Khanna12345678 <131513389+Khanna12345678@users.noreply.github.com>
2024-05-27 16:54:04 +0530

    add admin functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -57,7 +57,7 @@ export const signin = async (req, res, next) => {
     }
     
     const token = jwt.sign(
-      { id: validUser._id },
+      { id: validUser._id ,isAdmin:validUser.isAdmin},
       //yaha proceess.env.jwt secrethoga
       "bdububcubucb233"
     );

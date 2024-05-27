1cf01d755cfe94673e50cc5bb4f778d744b19ac9
Khanna12345678 <131513389+Khanna12345678@users.noreply.github.com>
2024-05-27 16:05:50 +0530

    complete delete user account functionality

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -2,12 +2,13 @@ import express from 'express';
 import { test } from '../controllers/user.controllers.js';
 import { verifyToken } from '../utils/verifyUser.js';
 import { updateUser } from '../controllers/user.controllers.js';
+import { deleteUser } from '../controllers/user.controllers.js';
 
 const router = express.Router();
 
 router.get('/test',test);
 router.put('/update/:userId', verifyToken, updateUser);
-// router.delete('/delete/:userId', verifyToken, deleteUser);
+router.delete('/delete/:userId', verifyToken, deleteUser);
 // router.post('/signout', signout);
 // router.get('/getusers', verifyToken, getUsers);
 // router.get('/:userId', getUser);
